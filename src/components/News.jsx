6ad0770cc471002b15973444c2656985c7d3b9d9
akash plackal <akashplackal@gmail.com>
2022-01-31 19:30:27 +0530

    homepage and cryptocurrencies page done

---
+++ src/components/News.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
+
+const News = () => {
+  return <div>News</div>;
+};
+
+export default News;
